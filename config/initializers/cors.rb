727a3d685cbd94d130ecaf38b6b3d16aad747734
Alex Kluew <dropbyalex@gmail.com>
2018-07-26 00:53:20 -0400

    Chore: add netlify link hardcoded for now

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,7 @@
 # :nodoc:
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', 'https://dailyvibesfrontend.netlify.com'
     resource  '*',
               headers: :any,
               methods: %i[get post put patch delete options],
