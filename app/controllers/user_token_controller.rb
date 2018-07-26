4ab4dcdd683b6558476362cac31c4a6f2922859d
Alex Kluew <dropbyalex@gmail.com>
2018-07-26 01:51:31 -0400

    Chore: hotfixes with env variables

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -6,7 +6,11 @@ class UserTokenController < Knock::AuthTokenController
 
   def dv_create
     # debugger
+
+    user = User.find_by(email: auth_params[:email])
+    auth_token = Knock::AuthToken.new payload: { sub: user.id }
     decoded_token = Knock::AuthToken.new(token: auth_token.token).payload
+
     return unless decoded_token && decoded_token['sub']
 
     result = {
@@ -15,5 +19,7 @@ class UserTokenController < Knock::AuthTokenController
     }.to_json
 
     render json: result, status: :created
+  rescue StandardError => e
+    raise e
   end
 end
